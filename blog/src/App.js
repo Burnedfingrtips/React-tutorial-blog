e8696424250854414e116a743157fdd2535639b4
mustafaksaeed <66759342+mustafaksaeed@users.noreply.github.com>
2024-10-27 07:57:48 -0400

    initialization  made homepage and navbar

---
+++ blog/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Navbar from "./Navbar";
+import HomePage from "./Homepage";
+function App() {
+
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+       <HomePage />
+        </div>
+    </div>
+  );
+}
+
+export default App;
